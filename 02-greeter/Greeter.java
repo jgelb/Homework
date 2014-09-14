0bb4017e7b9c9aafe3fb057d3f794143d68b1e8b
elc1798 <subliminalmau5@gmail.com>
2014-09-13 22:05:08 -0400

    Added Homework Comments

--- 02-greeter/Greeter.java
+++ 02-greeter/Greeter.java
@@ -1,8 +1,14 @@
 import java.io.*;
 import java.util.*;
 
+//COMMENTING SOMETHING FOR THE HW ASSIGNMENT
+
 public class Greeter {
     public void greet() {
 	System.out.println("Hello World!");
     }
 }
+//HW Comments below:
+//public static void main(String []args){
+	//Greeter.greet();	
+//}
