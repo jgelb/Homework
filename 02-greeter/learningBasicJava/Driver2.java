67e2de8b20b3f612c29c50c6b62469ed815f117d
elc1798 <subliminalmau5@gmail.com>
2014-09-18 08:41:10 -0400

    Updated to 9-18-14

---
+++ 02-greeter/learningBasicJava/Driver2.java
@@ -0,0 +1,15 @@
+public class Driver2 {
+	public static void main(String []args) {
+		Greeter g1 = new Greeter();
+		Greeter g2 , g3;
+		g2 = new Greeter();
+		g1.greet();
+		g2.greet();
+		g3 = g1;
+		System.out.println("g1: " + g1);
+		System.out.println("g2: " + g2);
+		System.out.println("g3: " + g3);
+		//String a = g1.greet.s();
+		//System.out.println(a);
+	}
+}
---
+++ 02-greeter/traditionalCodeStructuring/Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+	public static void main(String []args) {
+		Greeter g = new Greeter("abc123");
+		System.out.println(g.greet());
+	}
+}
