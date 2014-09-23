d4ada14bf6f03a5d7e59cdcd37f1c7565536ec93
jgelb <jgelb235@verizon.net>
2014-09-23 19:24:03 -0400

    Committed

---
+++ Coding_Bat/Driver.java
@@ -0,0 +1,11 @@
+public class Driver{
+    public static void main (String [] args){
+        StringStuff s1 = new StringStuff();
+	StringStuff s2 = new StringStuff();
+	StringStuff s3 = new StringStuff();
+	
+	System.out.println(s1.nonStart());
+	System.out.println(s2.makeAbba());
+	System.out.println(s3.diff21());
+    }
+}
