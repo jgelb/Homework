3c3060c94e57bed5621578a4062bc95a13807b19
elc1798 <subliminalmau5@gmail.com>
2014-09-23 12:26:21 -0400

    Added HW 05-codingBat

---
+++ 05-codingBat/Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+	public static void main(String []args) {
+		StringStuff x = new StringStuff();
+		System.out.println(x.nonStart("abc" , "DEF"));
+		System.out.println(x.makeAbba("POEM" , "Struct"));
+		System.out.println(x.diff21(10));
+		System.out.println(x.diff21(90));
+	}
+}
