c66240b08320704d84fe254bc3943fde1b266494
elc1798 <subliminalmau5@gmail.com>
2014-10-01 18:40:07 -0400

    Updated to 07-shapes

---
+++ 07-shapes/Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+	//Same as Driver.java
+	public static void main(String[] args){
+		Triangulate OBJ = new Triangulate();
+		System.out.println(OBJ.tri1(5));
+		System.out.println("");
+		System.out.println(OBJ.tri2(5));
+	}
+}
