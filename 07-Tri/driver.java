571f0158654d1e4c15a142c51e9f7e566a8f5284
jgelb <jgelb235@verizon.net>
2014-10-02 08:00:24 -0400

    Update driver.java

--- 07-Tri/driver.java
+++ 07-Tri/driver.java
@@ -1,7 +1,7 @@
 public class driver {
     public static void main(String[] args){
-	files g1 = new files();
-	files g2 = new files();
+	files g1 = new shapes();
+	files g2 = new shapes();
 	System.out.println (g1.Tri1(4));
 	System.out.println(g2.Tri2(4));
     }
