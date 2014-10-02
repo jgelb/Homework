869ab6b8ae7122f5421e3f7f782304305d1951c7
jgelb <jgelb235@verizon.net>
2014-10-01 21:40:47 -0400

    Done

---
+++ 07-Tri/driver.java
@@ -0,0 +1,8 @@
+public class driver {
+    public static void main(String[] args){
+	files g1 = new files();
+	files g2 = new files();
+	System.out.println (g1.Tri1(4));
+	System.out.println(g2.Tri2(4));
+    }
+}
