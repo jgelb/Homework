19aa21576a3d02434802ea4df1d7b20d1a5072c1
jgelb <jgelb235@verizon.net>
2014-10-02 18:48:25 -0400

    Done

---
+++ 7b/driver.java
@@ -0,0 +1,14 @@
+public class driver {
+    public static void main(String[] args){
+	files g1 = new files();
+	files g2 = new files();
+	files g3 = new files();
+	files g4 = new files();
+	files g5 = new files();
+	System.out.println (g1.Tri1(4));
+	System.out.println(g2.Tri2(4));
+	System.out.println(g3.Tri3(4));
+	System.out.println(g4.diamond(5));
+	System.out.println(g5.Tri4(4));
+    }
+}
