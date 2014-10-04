d77ddcbaac3e0ec7f4c299944a937b6f84b8651f
jgelb <jgelb235@verizon.net>
2014-10-04 13:33:37 -0400

    Done

---
+++ HW8-bat/driver.java
@@ -0,0 +1,8 @@
+public class driver{
+    public static void main (String [] args){
+	HW8  g1= new HW8();
+	HW8  g2= new HW8();
+	HW8  g2= new HW8();
+	System.out.println(g1.rectangle(10,50));
+	System.out.println(g2.stringSplosion("Coding"));
+    }}
