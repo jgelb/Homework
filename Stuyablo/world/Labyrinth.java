4388958d90b3fa793eec6e247f2abe7fe11d44f5
elc1798 <subliminalmau5@gmail.com>
2014-10-19 22:56:16 -0400

    Balancing changes

--- Stuyablo/world/Labyrinth.java
+++ Stuyablo/world/Labyrinth.java
@@ -29,7 +29,7 @@ public class Labyrinth extends World{
 		else if (chance < 100 && chance >= 67) {
 			m.golemTemplate("Golem" , lev);
 		}
-		m.setMaxHealth(m.maxHealth() + super.stage);
+		m.setMaxHealth(m.maxHealth() + (int)(super.stage / 2) + lev);
 		m.setHealth(m.maxHealth());
 		return m;
 	}
