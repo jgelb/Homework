86e8746ca3558814a839d6ff47d901aa445d79ca
elc1798 <subliminalmau5@gmail.com>
2014-10-26 13:47:27 -0400

    Revised file structure

--- 12-StuyabloPersonalVersion/Release1.1-Unstable/compile.sh
+++ 12-StuyabloPersonalVersion/Release1.1-Unstable/compile.sh
@@ -1,4 +1,13 @@
 cd assets
 pwd
+echo "Compiling java files..."
+mkdir COMPILED
+cp *.java COMPILED/
+cp mobs/*.java COMPILED/
+cp mobs/*.txt COMPILED/
+cp world/*.java COMPILED/
+cp world/*.txt COMPILED/
+cd COMPILED
 javac *.java
+rm *.java
 java Exec
