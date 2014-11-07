173651269afe78228958dbc6fa4e070f452de36f
elc1798 <subliminalmau5@gmail.com>
2014-11-07 08:42:39 -0500

    SArray.java

---
+++ 14-ArrayStuff/SArray.java
@@ -0,0 +1,68 @@
+public class SArray{
+	private Object[] data; //Should really be object[] data
+	public SArray(){
+		//Start array at size 16
+		data = new Object[16];
+		for (int i = 0; i < data.length; i++){
+			data[i] = null;
+		}
+	}
+	public Object[] add(Object obj){
+		//Adds object to end of array
+		if (size() == data.length){
+			Object[] buffer = new Object[data.length + 1];
+			for (int i = 0; i < data.length; i++){buffer[i] = data[i];}
+			buffer[data.length] = obj;
+			return buffer;
+		}
+		else {
+			data[size()] = obj;
+			return data;
+		}
+	}
+	public Object[] add(int index , Object obj){
+		//Adds object at index of array, shifts everything else over
+		try{
+			Object[] buffer = new Object[size() + 1];
+			for (int i = 0; i < buffer.length; i++){
+				if (i < index){
+					buffer[i] = data[i];
+				}
+				else if (i == index) {
+					buffer[i] = obj;
+				}
+				else {
+					buffer[i] = data[i - 1];
+				}
+			}
+			return buffer;
+		} catch(Exception e){e.printStackTrace()}
+	}
+	public Object get(int index){
+		//Gets object at index
+		return data[index];
+	}
+	public void set(int index , Object obj){
+		//Set object at index to obj
+		data[index] = obj;
+	}
+	public int size(){
+		//Returns length of array
+		int k = 0;
+		for (k = 0; k < data.length; k++){
+			if (data[k] == null){
+				return k;
+			}
+		}
+		return data.length; //Default case
+	}
+	public Object[] remove(int index){
+		//Removes object at index, shifts everything over
+		
+	}
+/*
+	public Object[] pop(int index){
+		//Removes object , turns it to null
+	}
+*/
+}
