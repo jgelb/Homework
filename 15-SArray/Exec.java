8b68b7f0c4a3e05b6383710799e14920a8a39445
elc1798 <subliminalmau5@gmail.com>
2014-11-09 14:26:28 -0500

    Updated test cases

--- 15-SArray/Exec.java
+++ 15-SArray/Exec.java
@@ -3,16 +3,21 @@ public class Exec{
 		SArray test = new SArray(32); //Should create a 32 element array
 		test.print();
 		System.out.println(test.size());
-		test.add("TEST");
-		test.add("FOO");
-		test.add("YOLO");
-		test.add(9001);
-		test.add(true);
-		test.add(false);
+		test.append("TEST");
+		test.append("FOO");
+		test.append("YOLO");
+		test.append(9001);
+		test.append(true);
+		test.append(false);
 		test.print();
 		System.out.println(test.size());
 		System.out.println(test.get(0));
 		test.remove(0);
+		test.print();
+		System.out.println(test.size());
+		for (int i = 0; i < 16; i++){
+			test.replace(i , "Some object with value " + i);
+		}
 		test.print();
 		System.out.println(test.size());
 	}
