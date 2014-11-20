925396340544b7ea6f3c4256e7751090f15df228
elc1798 <subliminalmau5@gmail.com>
2014-11-20 08:05:28 -0500

    Oops forgot to commit yesterday

--- 18-WordSearch/Wordsearch.java
+++ 18-WordSearch/Wordsearch.java
@@ -52,6 +52,41 @@ public class Wordsearch{
 
 	public getWordsFromUserInput(){
 		Scanner sc = new Scanner(System.in);
+		while (!sc.nextLine().equals("STOP_INPUT")){
+			words.add(sc.nextLine();
+		}
+	}
+
+	// Create the wordsearch:
+
+/*
+	There needs to be some set of rules to start inputting words from ArrayList<String> words into the data.
+	NOT NEEDED: checkOverlap(int row, int column, String word) Will return true if it can overlap correctly
+	insertWord(String word, int row, int column , int direction) will first check overlap, if true, insert the word in given direction
+*/
+
+	public void insertWord (int row, int column, String word , int direction){
+		boolean response = true;
+		switch (direction){
+			case 1://Horizontally left to right
+				response = response && (row > board.length);
+				response = response && (column + word.length() > board[row].length);
+				if (response){
+					for (int i = column; i < column + word.length(); i++){
+						response = response && (board[row][i].equals(".") || word.charAt(i - column)); 
+					}
+				}
+				if (response){
+					for (int i = column; i < column + word.length(); i++){
+						response = response && (board[row][i].equals(".") || word.charAt(i - column)); 
+					}
+				}
+				break;
+			case 2:
+				break;
+			default:
+				break;
+		}
 	}
 
 	public static void main(String[] args){
