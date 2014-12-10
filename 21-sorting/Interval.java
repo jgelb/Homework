ef56c0b03152657bd5007a494bd7580a87f2548e
jgelb <jgelb235@verizon.net>
2014-12-09 20:21:25 -0500

    done

---
+++ 21-sorting/Interval.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Interval {
+    int low,high;
+    public Interval(){
+    
+    boolean check = false;
+    Random r = new Random();
+    low = r.nextInt(100);
+    while (check == false){
+        int x = r.nextInt(100);
+        if(x >= low) {
+            high = x + 1;
+            check = true;
+        }
+    }
+    }
+    public String toString(){
+    return "[ " + low + ", " + high + " ] ";
+    }
+    public int getLow(){
+	return low;
+    }
+    public int getHigh(){
+	return high;
+    }
+   
+    public int compareTo(Interval other){
+	return this.getLow()-other.getLow();
+	}
+
+	    
+
+    
+    public static void main(String[] args){
+    Interval k = new Interval();
+    System.out.println(k);
+    Interval[] lists = new Interval[10];
+    int i = 0;
+    while (i < 10){
+        lists[i] = new Interval();
+        i ++;
+    }
+    System.out.println(Arrays.toString(lists));
+       
+    }}
