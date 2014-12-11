6377d566212963d56c5f3d6990f70e350213f0bf
jgelb <jgelb235@verizon.net>
2014-12-10 22:46:14 -0500

    done

---
+++ 12-Stuyablo/Monster.java
@@ -0,0 +1,13 @@
+public class Monster extends Basechar{
+    int healthboost=(int)Math.ceil(Math.random()*500)+200;
+    public Monster (String n) {
+	super(n);
+	this.health = this.health + healthboost;
+	this.attack = this.attack + (int)Math.ceil(Math.random()*5);
+    }
+    /*
+      public void Attack (Basechar other) {
+      System.out.println(this + " is attacking " + other + "!!!");
+      }
+    */
+}
